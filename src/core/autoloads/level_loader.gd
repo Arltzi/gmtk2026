6f2703867da5a83df54de072bc925411643f07f5
Lev Zitron <lev@zitron.ca>
2026-07-25 11:12:40 -0700

    adjust scales

--- src/core/autoloads/level_loader.gd
+++ src/core/autoloads/level_loader.gd
@@ -28,19 +28,20 @@ signal level_loaded(level: Level)
 # static, public, protected, private
 
 func _enter_tree() -> void:
-  load_level(initial_level)
+	load_level(initial_level)
   
 func load_level(scene: PackedScene) -> Level:
-  var new_level: Level = scene.instantiate()
-  add_child(new_level)
-  
-  new_level.completed.connect(on_level_completed)
-  new_level.start_level()
-  level_loaded.emit(new_level)
-  return null
+	var new_level: Level = scene.instantiate()
+	add_child(new_level)
+
+	new_level.completed.connect(on_level_completed)
+	new_level.start_level()
+	level_loaded.emit(new_level)
+	return null
   
 func on_level_completed(level: Level) -> void:
-  level.queue_free()
-  load_level(level.next_level_scene)
-  pass
+	if(level.next_level_scene != null):
+		level.queue_free()
+		load_level(level.next_level_scene)
+	pass
 #endregion
