3f2893f49fbb5a7118f93e5a6f22dbec0da52277
idan gomberg <idangomberg@gmail.com>
2026-07-26 00:46:07 -0400

    Added Clock Visual Scene and to the main level

--- src/gameplay/player/player.gd
+++ src/gameplay/player/player.gd
@@ -5,17 +5,17 @@ signal collectibles_updated(new_val: int)
 signal collectibles_consumed(count: int)
 
 var collectibles: int = 0:
-  get:
-    return collectibles
-  set(value):
-    collectibles = value
-    collectibles_updated.emit(value)
+	get:
+		return collectibles
+	set(value):
+		collectibles = value
+		collectibles_updated.emit(value)
 
 func on_pickup(_collectible: Collectible) -> void:
-  collectibles += 1
-  pass
+	collectibles += 1
+	pass
   
 func reset_counter() -> void:
-  collectibles_consumed.emit(collectibles)
-  collectibles = 0
-  pass
+	collectibles_consumed.emit(collectibles)
+	collectibles = 0
+	pass
